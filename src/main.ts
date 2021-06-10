7b7544c0437cc1df274bae6b48a20d9870ef541e
knz21 <cismollre@gmail.com>
2021-06-10 23:21:41 +0900

    multi bot, bugfix

--- src/main.ts
+++ src/main.ts
@@ -4,40 +4,46 @@ const prepareMessages = () => {
     const holidaysSheet = SpreadsheetApp.openById('1dD14Up7ialGPRQmTb74zgxbctB9CaUsYa41aVIHxfJ4').getSheetByName('public holiday')
     const holidays = holidaysSheet.getRange(1, 1, holidaysSheet.getLastRow()).getValues().map(row => row[0])
     const channelMaster: string[][] = getAllValues(messageSheets, 'master_channel')
+    const botMaster: string[][] = getAllValues(messageSheets, 'master_bot')
     const userMaster: string[][] = getAllValues(messageSheets, 'master_user')
     const groupMaster: string[][] = getAllValues(messageSheets, 'master_group')
     const preparedSheet = messageSheets.getSheetByName('prepared')
     const preparingMessages: any[][] = []
 
-    const createMessage = (channel, user, group, message): { [key: string]: string } => {
+    const createMessage = (channel, bot, user, group, message): { [key: string]: string } => {
         const channelId = channelMaster.find(channelRow => channelRow[0] == channel)?.[1]
+        const botName = botMaster.find(botRow => botRow[0] == bot)?.[0]
         const userId = userMaster.find(userRow => userRow[0] == user)?.[1]
-        const groupId = userMaster.find(groupRow => groupRow[0] == group)?.[1]
+        const groupId = groupMaster.find(groupRow => groupRow[0] == group)?.[1]
         const text = `${userId ? `<@${userId}> ` : ''}${groupId ? `<!subteam^${groupId}> ` : ''}${message}`
         return {
             channelId: channelId,
+            botName: botName,
             text: text,
         }
     }
 
     const prepareRegularMessages = () => {
-        const isValidRow = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, user?, group?, message?): boolean => {
+        const isValidRow = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, bot?, user?, group?, message?): boolean => {
             if (day == '' || day == 'なし') {
                 Logger.log('day: empty or なし')
                 return false
             }
-            if (times != '' && times == 0) {
+            if (typeOf(times) == 'number' && times < 1) {
                 Logger.log('times: 0')
                 return false
             }
-            return isValidMessage(hour, minute, channel, message)
+            return isValidMessage(hour, minute, channel, bot, message)
         }
         const isHoliday = (): boolean => {
-            const holidays = holidaysSheet.getRange(1, 1, holidaysSheet.getLastRow()).getValues().map(row => row[0])
             const today = new Date()
-            return holidays.some(holiday => holiday.getFullYear() == today.getFullYear() && holiday.getMonth() == today.getMonth() && holiday.getDate() == today.getDate())
+            const isHoliday: boolean = holidays.some(holiday => holiday.getFullYear() == today.getFullYear() && holiday.getMonth() == today.getMonth() && holiday.getDate() == today.getDate())
+            if (isHoliday) {
+                Logger.log('is holiday')
+            }
+            return isHoliday
         }
-        const isTheDayOfTheWeek = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, user?, group?, message?): boolean => {
+        const isTheDayOfTheWeek = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, bot?, user?, group?, message?): boolean => {
             const dayOfTheWeek = new Date().getDay()
             const days = ['日', '月', '火', '水', '木', '金', '土']
             if (day == days[dayOfTheWeek]) return true
@@ -45,20 +51,24 @@ const prepareMessages = () => {
             Logger.log('today is not the day of the week')
             return false
         }
-        const isSkip = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, user?, group?, message?): boolean => {
-            if (skip != '' && skip > 0) {
+        const isSkip = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, bot?, user?, group?, message?): boolean => {
+            if (typeOf(skip) == 'number' && skip > 0) {
                 Logger.log('skip')
                 return true
             }
             return false
         }
-        const prepare = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, user?, group?, message?): any[] => {
-            const { channelId, text } = createMessage(channel, user, group, message)
+        const prepare = (day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, bot?, user?, group?, message?): any[] => {
+            const { channelId, botName, text } = createMessage(channel, bot, user, group, message)
             if (!channelId) {
                 Logger.log('found no channel id')
                 return null
             }
-            if (preparedMessages.some(messageRow => messageRow[1] == channelId && messageRow[2] == text)) {
+            if (!botName) {
+                Logger.log('found no bot')
+                return null
+            }
+            if (preparedMessages.some(messageRow => messageRow[1] == channelId && messageRow[2] == botName && messageRow[3] == text)) {
                 Logger.log('already prepared')
                 return null
             }
@@ -67,24 +77,27 @@ const prepareMessages = () => {
             date.setMinutes(minute)
             const trigger = ScriptApp.newTrigger('sendMessage').timeBased().at(date).create()
             Logger.log('regular message prepared')
-            return [trigger.getUniqueId(), channelId, text, date]
+            return [trigger.getUniqueId(), channelId, botName, text, date]
         }
-
-        const updateTimes = (index: number, day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, user?, group?, message?) => {
+        const updateTimes = (index: number, day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, bot?, user?, group?, message?) => {
             if (times != '' && times > 0) {
-                const newRow = [day, hour, minute, times - 1, skip, skipPeriod, channel, user, group, message]
+                const newRow = [day, hour, minute, times - 1, skip, skipPeriod, channel, bot, user, group, message]
                 regularSheet.getRange(index + 2, 1, 1, newRow.length).setValues([newRow])
             }
         }
-
-        const updateSkips = (index: number, day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, user?, group?, message?) => {
+        const updateSkips = (index: number, day?, hour?, minute?, times?, skip?, skipPeriod?, channel?, bot?, user?, group?, message?) => {
             const newSkip = skip - 1
-            const newRow = [day, hour, minute, times, newSkip == 0 ? skipPeriod : newSkip, skipPeriod, channel, user, group, message]
+            const newRow = [day, hour, minute, times, newSkip == 0 ? skipPeriod : newSkip, skipPeriod, channel, bot, user, group, message]
             regularSheet.getRange(index + 2, 1, 1, newRow.length).setValues([newRow])
         }
 
         const regularSheet = messageSheets.getSheetByName('regular')
-        const regularMessages = regularSheet.getRange(2, 1, regularSheet.getLastRow() - 1, regularSheet.getLastColumn()).getValues()
+        const lastRowNumber = regularSheet.getLastRow()
+        if (lastRowNumber <= 1) {
+            Logger.log('no regular message')
+            return
+        }
+        const regularMessages = regularSheet.getRange(2, 1, lastRowNumber - 1, regularSheet.getLastColumn()).getValues()
         const lastPreparedRowNumber = preparedSheet.getLastRow()
         const preparedMessages: any[][] = lastPreparedRowNumber > 0 ? preparedSheet.getRange(1, 1, lastPreparedRowNumber, preparedSheet.getLastColumn()).getValues() : []
         regularMessages.forEach((row: any[], index: number) => {
@@ -103,7 +116,7 @@ const prepareMessages = () => {
     }
 
     const prepareSingleMessage = () => {
-        const isToday = (date?, hour?, minute?, channel?, user?, group?, message?): boolean => {
+        const isToday = (date?, hour?, minute?, channel?, bot?, user?, group?, message?): boolean => {
             const today = new Date()
             const isToday = typeOf(date) == 'date' && date.getFullYear() == today.getFullYear() && date.getMonth() == today.getMonth() && date.getDate() == today.getDate()
             if (!isToday) {
@@ -111,20 +124,24 @@ const prepareMessages = () => {
             }
             return isToday
         }
-        const isValidRow = (date?, hour?, minute?, channel?, user?, group?, message?): boolean => {
-            return isValidMessage(hour, minute, channel, message)
+        const isValidRow = (date?, hour?, minute?, channel?, bot?, user?, group?, message?): boolean => {
+            return isValidMessage(hour, minute, channel, bot, message)
         }
-        const prepare = (date?, hour?, minute?, channel?, user?, group?, message?): any[] => {
-            const { channelId, text } = createMessage(channel, user, group, message)
+        const prepare = (date?, hour?, minute?, channel?, bot?, user?, group?, message?): any[] => {
+            const { channelId, botName, text } = createMessage(channel, bot, user, group, message)
             if (!channelId) {
                 Logger.log('found no channel id')
+                return null
+            }
+            if (!botName) {
+                Logger.log('found no bot')
                 return null
             }
             date.setHours(hour)
             date.setMinutes(minute)
             const trigger = ScriptApp.newTrigger('sendMessage').timeBased().at(date).create()
             Logger.log('single message prepared')
-            return [trigger.getUniqueId(), channelId, text, date]
+            return [trigger.getUniqueId(), channelId, botName, text, date]
         }
         const deleteRow = (index: number) => {
             singleSheet.deleteRow(index + 2)
@@ -132,7 +149,12 @@ const prepareMessages = () => {
         }
 
         const singleSheet = messageSheets.getSheetByName('single')
-        const singleMessages = singleSheet.getRange(2, 1, singleSheet.getLastRow() - 1, singleSheet.getLastColumn()).getValues()
+        const lastRowNumber = singleSheet.getLastRow()
+        if (lastRowNumber <= 1) {
+            Logger.log('no single message')
+            return
+        }
+        const singleMessages = singleSheet.getRange(2, 1, lastRowNumber - 1, singleSheet.getLastColumn()).getValues()
         singleMessages.forEach((row: any[], index: number) => {
             if (isToday(...row) && isValidRow(...row)) {
                 const preparingMessage: any[] = prepare(...row)
@@ -145,10 +167,12 @@ const prepareMessages = () => {
     }
 
     try {
+        Logger.log('start prepare messages')
         prepareRegularMessages()
         prepareSingleMessage()
     } finally {
         if (preparingMessages.length == 0) return
+        Logger.log('write prepare messages')
         preparedSheet.getRange(preparedSheet.getLastRow() + 1, 1, preparingMessages.length, preparingMessages[0].length).setValues(preparingMessages)
     }
 }
@@ -158,7 +182,7 @@ const getAllValues = (sheets, sheetName: string): string[][] => {
     return sheet.getRange(1, 1, sheet.getLastRow(), sheet.getLastColumn()).getDisplayValues()
 }
 
-const isValidMessage = (hour, minute, channel, message): boolean => {
+const isValidMessage = (hour, minute, channel, bot, message): boolean => {
     if (typeOf(hour) != 'number' || hour < 0 || hour > 23) {
         Logger.log('hour: invalid')
         return false
@@ -171,11 +195,14 @@ const isValidMessage = (hour, minute, channel, message): boolean => {
         Logger.log('empty channel')
         return false
     }
+    if (bot == '') {
+        Logger.log('empty bot')
+        return false
+    }
     if (message == '') {
         Logger.log('empty message')
         return false
     }
-
     return true
 }
 
@@ -187,9 +214,10 @@ const sendMessage = (e: object) => {
     const messages: string[][] = sheet.getRange(1, 1, lastRowNumber, sheet.getLastColumn()).getDisplayValues()
     const message: string[] = messages.find(message => message[0] == triggerUid)
     const channelId: string = message[1]
-    const text: string = message[2]
+    const botName: string = message[2]
+    const text: string = message[3]
     if (channelId.length == 0 || text.length == 0) return
-    const token = PropertiesService.getScriptProperties().getProperty('slack_user_token')
+    const token = PropertiesService.getScriptProperties().getProperty(`slack_${botName}_token`)
     const formData = {
         token: token,
         channel: channelId,
